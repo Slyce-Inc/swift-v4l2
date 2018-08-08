454dce4a98668da8b47cf6bc12eee722a95438b4
Jason Allum <jallum@tonicdesign.com>
2018-08-08 07:10:08 -0400

    Added YUV420 color format.

--- shim.h
+++ shim.h
@@ -14,4 +14,4 @@ EXPORT(V4L2_PIX_FMT_RGB24, 	unsigned int)
 EXPORT(V4L2_PIX_FMT_NV12, 	unsigned int)
 EXPORT(V4L2_PIX_FMT_RGB32, 	unsigned int)
 EXPORT(V4L2_PIX_FMT_YUYV, 	unsigned int)
-
+EXPORT(V4L2_PIX_FMT_YUV420, unsigned int)
