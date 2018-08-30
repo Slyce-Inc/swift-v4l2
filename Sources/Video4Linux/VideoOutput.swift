973957a0a6ba7dda2428222a929684378062c7a4
Jason Allum <jallum@tonicdesign.com>
2018-08-30 16:03:57 -0400

    Updated VideoOutput to use memory-mapped buffers, saving some copies.

--- Sources/Video4Linux/VideoOutput.swift
+++ Sources/Video4Linux/VideoOutput.swift
@@ -12,8 +12,36 @@ public class VideoOutput {
     self.frameSizeInBytes = frameSizeInBytes
   }
 
+  public func startStreaming() throws {
+    for i in 0 ..< buffers.count {
+      var buf = v4l2_buffer()
+      buf.type = V4L2_BUF_TYPE_VIDEO_OUTPUT.rawValue
+      buf.memory = V4L2_MEMORY_MMAP.rawValue
+      buf.index = UInt32(i)
+
+      if -1 == ioctl(device.fileDescriptor, _VIDIOC_QBUF, &buf) {
+        throw VideoDeviceError.UnableToQueueBuffer(device:device)
+      }
+    }
+
+    var type = V4L2_BUF_TYPE_VIDEO_OUTPUT.rawValue
+    if -1 == ioctl(device.fileDescriptor, _VIDIOC_STREAMON, &type) {
+      throw VideoDeviceError.UnableToEnableStreaming(device:device)
+    }
+  }
+
   public func write(frame: UnsafeRawPointer) throws {
     Glibc.write(self.device.fileDescriptor, frame, self.frameSizeInBytes)
+  }
+
+  public func withFrameBuffer(_ block: (UnsafeMutableRawPointer, Int) -> ()) throws {
+    var buf = v4l2_buffer()
+    buf.type = V4L2_BUF_TYPE_VIDEO_OUTPUT.rawValue
+    buf.memory = V4L2_MEMORY_MMAP.rawValue
+    if -1 != ioctl(self.device.fileDescriptor, _VIDIOC_DQBUF, &buf) {
+      block(self.buffers[Int(buf.index)].baseAddress, Int(buf.bytesused))
+      _ = ioctl(self.device.fileDescriptor, _VIDIOC_QBUF, &buf)
+    }
   }
 
   deinit {
@@ -25,7 +53,7 @@ public class VideoOutput {
 
 
 public extension VideoDevice {
-  public func startOutput(width:Int, height:Int, pixelFormat:PixelFormat) throws -> VideoOutput {
+  public func startOutput(width:Int, height:Int, pixelFormat:PixelFormat, numberOfBuffers:Int = 8) throws -> VideoOutput {
     var v = v4l2_format()
     v.type = V4L2_BUF_TYPE_VIDEO_OUTPUT.rawValue
     guard ioctl(self.fileDescriptor, _VIDIOC_G_FMT, &v) != -1 else {
@@ -41,6 +69,40 @@ public extension VideoDevice {
       throw VideoDeviceError.UnableToWriteFormatToDevice(device:self)
     }
 
-    return VideoOutput(device:self, buffers:[], frameSizeInBytes:frameSizeInBytes)
+    var req = v4l2_requestbuffers()
+    req.type = V4L2_BUF_TYPE_VIDEO_OUTPUT.rawValue
+    req.memory = V4L2_MEMORY_MMAP.rawValue
+    req.count = UInt32(numberOfBuffers)
+    guard -1 != ioctl(self.fileDescriptor, _VIDIOC_REQBUFS, &req) else {
+      throw VideoDeviceError.DeviceIsNotCapableOfMemoryMapping(device:self)
+    }
+
+    if Int(req.count) < numberOfBuffers {
+      throw VideoDeviceError.UnableToAllocateEnoughBuffers(device:self, numberOfBuffers:numberOfBuffers)
+    }
+
+    var buffers: [VideoFrameBuffer] = []
+    for i in 0 ..< Int(req.count) {
+      var buf = v4l2_buffer()
+      buf.type = V4L2_BUF_TYPE_VIDEO_OUTPUT.rawValue
+      buf.memory = V4L2_MEMORY_MMAP.rawValue
+      buf.index = UInt32(i)
+      guard -1 != ioctl(self.fileDescriptor, _VIDIOC_QUERYBUF, &buf) else {
+        throw VideoDeviceError.UnableToQueryBuffer(device:self)
+      }
+
+      guard let baseAddress = mmap(nil,
+        Int(buf.length),
+        PROT_READ | PROT_WRITE,
+        MAP_SHARED,
+        self.fileDescriptor,
+        __off_t(buf.m.offset)
+      ), baseAddress != MAP_FAILED else {
+        throw VideoDeviceError.UnableToMapBuffer(device:self)
+      }
+      buffers.append(VideoFrameBuffer(baseAddress:baseAddress, length:Int(buf.length)))
+    }
+
+    return VideoOutput(device:self, buffers:buffers, frameSizeInBytes:frameSizeInBytes)
   }
 }
