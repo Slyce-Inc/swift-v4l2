24e043af9dbb4069fdaa1bca49151acb6261e0b6
Jason Allum <jason@slyce.it>
2019-11-22 20:28:40 +0000

    Updated to Swift 5.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -6,18 +6,14 @@ let package = Package(
   name: "Video4Linux",
   products: [
     .library(name: "Video4Linux", targets: ["Video4Linux"]),
-    .library(name: "Clibv4l2", targets: ["Clibv4l2"])
-  ],
-  dependencies: [
   ],
   targets: [
     .target(
-      name: "Clibv4l2",
-      dependencies: []
-    ),
-    .target(
       name: "Video4Linux",
       dependencies: ["Clibv4l2"]
+    ),
+    .systemLibrary(
+      name: "Clibv4l2"
     ),
   ]
 )
